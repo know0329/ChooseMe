74471e4e6a63d0b1854f110960500d91e7d43dd7
I-MO <know0329@naver.com>
2024-04-03 15:21:51 +0900

    start

---
+++ Android/ChooseMe/app/src/main/java/com/example/chooseme/ui/theme/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.example.chooseme.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier
+){
+
+}
